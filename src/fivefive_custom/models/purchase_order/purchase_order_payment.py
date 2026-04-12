b91b35fd08a801eec195b85a1d8f8a8e2cac81d6
algnot <thanawat.tongla@gmail.com>
2026-04-13 00:21:17 +0700

    fix up

--- src/fivefive_custom/models/purchase_order/purchase_order_payment.py
+++ src/fivefive_custom/models/purchase_order/purchase_order_payment.py
@@ -51,8 +51,8 @@ class PurchaseOrderPayment(models.Model):
 
     def action_open_cancel_payment_wizard(self):
         self.ensure_one()
-        if self.purchase_order_id.state == "clearing":
-            raise UserError("ไม่สามารถยกเลิก Payment ที่อยู่ใน status Clearing ได้")
+        if self.purchase_order_id.state not in ["draft", "po_issued", "documents_completed"]:
+            raise UserError(f"ไม่สามารถยกเลิก Payment ที่อยู่ใน status {self.purchase_order_id.state} ได้")
         if self.is_cancel:
             raise UserError("Payment นี้ถูกยกเลิกไปแล้ว")
         if float_compare(self.amount_usd, 0, precision_digits=2) <= 0:
@@ -73,8 +73,8 @@ class PurchaseOrderPayment(models.Model):
 
     def action_cancel_payment(self, reason, cancel_attachment=False, cancel_attachment_name=False):
         for payment in self:
-            if payment.purchase_order_id.state == "clearing":
-                raise UserError("ไม่สามารถยกเลิก Payment ที่อยู่ใน status Clearing ได้")
+            if payment.purchase_order_id.state not in ["draft", "po_issued", "documents_completed"]:
+                raise UserError(f"ไม่สามารถยกเลิก Payment ที่อยู่ใน status {self.purchase_order_id.state} ได้")
 
             if payment.is_cancel:
                 raise UserError("Payment นี้ถูกยกเลิกไปแล้ว")
